6701922283d84e6ba2f78d743135d120d1cf20ea
Junior <kiala231.unicaen.fr>
2025-09-23 11:52:45 +0200

    suite de projet solo

--- scripts.js
+++ scripts.js
@@ -1,2 +1,7 @@
 const canvasWidth = 400;
 const canvasHeight = 700;
+
+const canvas = document.getElementById("pong");
+const btnStart= document.getElementById("btnStart");
+const rigth = document.getElementById("gauche");
+const left = document.getElementById("droite");
